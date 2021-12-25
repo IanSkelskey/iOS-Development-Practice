52705cc6da07c2bc5b5521187d490e98d76797f1
Ian Skelskey <IanSkelskey@gmail.com>
2021-12-25 18:25:11 -0500

    WorldTrotter 1.0
    Completed primary assignment for Chapter 3: Views and View Hierarchy. No challenges have been implemented.

---
+++ WorldTrotter/WorldTrotter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  WorldTrotter is a unit conversion app.
+//  Initially, it will only convert degrees
+//      Fahrenheit (F) to degrees Celcius (C)
+//
+//  Created by Ian Skelskey on 12/23/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+
+
+}
+
